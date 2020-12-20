ca4aebb2942645e497234f31caddb9f1c18c41aa
Muhammadyusuf <muhammadyusuf.kurbonov2002@gmail.com>
2020-12-20 20:38:54 +0500

    Response is being send

--- pages/api/printReport.js
+++ pages/api/printReport.js
@@ -7,39 +7,42 @@ export default (req, res) => {
     let signature = CryptoJS.SHA256("CTD378Du" + MerID + OrderID).toString(CryptoJS.enc.Base64)
 
     axios.default.post("https://mpi.mkb.ru:8443/OnlineReceipt/1/" + MerID + "/receipt",
-            {
-                "id": OrderID,
-                "orderId": OrderID,
-                "client": {"email": "yumecs.pay@gmail.com"},
-                "company": {
-                    "email": "yumecs.pay@gmail.com",
-                    "inn": "7726433751",
-                    "paymentAddress": "https://yumecs.uz"
-                },
-                "receipt": {
-                    "items": [{
-                        "name": OrderID + " " + fio,
-                        "price": amt,
-                        "quantity": 1.0,
-                        "sum": amt,
-                        "unit": "шт",
-                        "method": "full_payment",
-                        "object": "commodity",
-                        "vat": {"type": "none", "sum": 0}
-                    }],
-                    "payments": [{"type": 1, "sum": amt}],
-                    "total": amt
-                }
+        {
+            "id": OrderID,
+            "orderId": OrderID,
+            "client": {"email": "yumecs.pay@gmail.com"},
+            "company": {
+                "email": "yumecs.pay@gmail.com",
+                "inn": "7726433751",
+                "paymentAddress": "https://yumecs.uz"
             },
-            {
-                headers: {
-                    "Content-Type": "application/json",
-                    "Signature": signature
-                }
-            }).then(function (response) {
-                console.log(response)
-        }).catch(function (error) {
-            console.log(error)
-        })
-    res.statusCode = 200
+            "receipt": {
+                "items": [{
+                    "name": OrderID + " " + fio,
+                    "price": amt,
+                    "quantity": 1.0,
+                    "sum": amt,
+                    "unit": "шт",
+                    "method": "full_payment",
+                    "object": "commodity",
+                    "vat": {"type": "none", "sum": 0}
+                }],
+                "payments": [{"type": 1, "sum": amt}],
+                "total": amt
+            }
+        },
+        {
+            headers: {
+                "Content-Type": "application/json",
+                "Signature": signature
+            }
+        }).then(function (response) {
+        console.log(response)
+        res.statusCode = 200
+        res.send(response.data)
+    }).catch(function (error) {
+        res.statusCode = 500
+        res.send(error)
+        console.log(error)
+    })
 }
