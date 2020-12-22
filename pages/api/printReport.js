7c32093217e08ba9e0edceb409bc2b2fcacd44b8
Muhammadyusuf <muhammadyusuf.kurbonov2002@gmail.com>
2020-12-22 18:49:48 +0500

    Loging improved #2

--- pages/api/printReport.js
+++ pages/api/printReport.js
@@ -49,14 +49,28 @@ export default (req, res) => {
                 "Signature": signature
             }
         }).then(function (response) {
-        console.log(response.data)
+            console.log("Response: \n")
+        console.log(JSON.stringify(response.data))
         res.statusCode = 200
-        res.send(response.data)
+        res.send({
+            status: "Send successful"
+        })
     }).catch(function (error) {
-        console.log("Error:")
-        console.error(JSON.stringify(error.stack))
-        console.log(JSON.stringify(error))
-        res.statusCode = 200
-        res.send(error)
-    })
+        if (error.response) {
+            // The request was made and the server responded with a status code
+            // that falls out of the range of 2xx
+            console.log(error.response.data);
+            console.log(error.response.status);
+            console.log(error.response.headers);
+        } else if (error.request) {
+            // The request was made but no response was received
+            // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+            // http.ClientRequest in node.js
+            console.log(error.request);
+        } else {
+            // Something happened in setting up the request that triggered an Error
+            console.log('Error', error.message);
+        }
+        console.log(error.config);
+    });
 }
