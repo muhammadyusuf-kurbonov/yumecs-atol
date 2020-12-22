c8c87146d90e8bdaaaab8b63be8ba5b0a27f5b2c
Muhammadyusuf <muhammadyusuf.kurbonov2002@gmail.com>
2020-12-22 15:56:18 +0500

    Loging improved

--- pages/api/printReport.js
+++ pages/api/printReport.js
@@ -4,38 +4,45 @@ export default (req, res) => {
 
     let {OrderID, amt, fio, MerID} = req.body
 
-    console.log(req)
+    console.log(req.body)
 
     let signature = CryptoJS.SHA256("CTD378Du" + MerID + OrderID).toString(CryptoJS.enc.Base64)
 
     var amount = (parseFloat(amt) / 100.0).toFixed(2)
 
-    axios.default.post("https://mpi.mkb.ru:8443/OnlineReceipt/1/" + MerID + "/receipt",
-        {
-            "id": OrderID,
-            "orderId": OrderID,
-            "receipt_id": OrderID,
-            "client": {"email": "yumecs.pay@gmail.com"},
-            "company": {
-                "email": "yumecs.pay@gmail.com",
-                "inn": "7726433751",
-                "paymentAddress": "https://yumecs.uz"
-            },
-            "receipt": {
-                "items": [{
-                    "name": OrderID + " " + fio,
-                    "price": amount,
-                    "quantity": 1.0,
-                    "sum": amount,
-                    "unit": "шт",
-                    "method": "full_payment",
-                    "object": "commodity",
-                    "vat": {"type": "none", "sum": 0}
-                }],
-                "payments": [{"type": 1, "sum": amount}],
-                "total": amount
-            }
+    let data = {
+        "id": OrderID,
+        "orderId": OrderID,
+        "receipt_id": OrderID,
+        "client": {"email": "yumecs.pay@gmail.com"},
+        "company": {
+            "email": "yumecs.pay@gmail.com",
+            "inn": "7726433751",
+            "paymentAddress": "https://yumecs.uz"
         },
+        "receipt": {
+            "items": [{
+                "name": OrderID + " " + fio,
+                "price": amount,
+                "quantity": 1.0,
+                "sum": amount,
+                "unit": "шт",
+                "method": "full_payment",
+                "object": "commodity",
+                "vat": {"type": "none", "sum": 0}
+            }],
+            "payments": [{"type": 1, "sum": amount}],
+            "total": amount
+        }
+    };
+
+    console.log("Data\n")
+    console.log(data)
+    console.log("======================================")
+
+
+    axios.default.post("https://mpi.mkb.ru:8443/OnlineReceipt/1/" + MerID + "/receipt",
+        data,
         {
             headers: {
                 "Content-Type": "application/json",
@@ -46,7 +53,9 @@ export default (req, res) => {
         res.statusCode = 200
         res.send(response.data)
     }).catch(function (error) {
+        console.log("Error:")
         console.error(JSON.stringify(error.stack))
+        console.log(JSON.stringify(error))
         res.statusCode = 200
         res.send(error)
     })
