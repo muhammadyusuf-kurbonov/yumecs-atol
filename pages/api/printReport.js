c8d8c3c82efb79d51f15ef54e6c6f4090e49b8d3
Muhammadyusuf <muhammadyusuf.kurbonov2002@gmail.com>
2020-12-23 22:09:28 +0500

    New send algorithm #6

--- pages/api/printReport.js
+++ pages/api/printReport.js
@@ -83,7 +83,7 @@ export default async (req, res) => {
         let paymentResponse = await axios.default.post("https://mpi.mkb.ru/MPI_payment/",
             payment_info)
 
-        await res.sendHTML(await paymentResponse.data)
+        await res.send((await paymentResponse).data)
 
         console.log(JSON.stringify(paymentResponse.data))
     }else{
