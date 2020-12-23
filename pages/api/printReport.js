9cfdb8fff00689f0b2c4260c6177658fd79b337a
Muhammadyusuf <muhammadyusuf.kurbonov2002@gmail.com>
2020-12-23 21:46:13 +0500

    New send algorithm

--- pages/api/printReport.js
+++ pages/api/printReport.js
@@ -1,90 +1,92 @@
-export default (req, res) => {
+export default async (req, res) => {
     let axios = require("axios")
     let CryptoJS = require("crypto-js")
 
-    if (req.body["ResponseCode"] === "1") {
-        let {OrderID, amt, fio, MerID} = req.body
+    const mid = "620000000003267"
+    const aid = "443222"
+    const site_link = "https://yumecs.uz"
+    const redirect_url = "www.yumecs.uz"
+    const merchant_mail = "yumecspay@gmail.com"
 
-        console.log(req.body)
 
-        let signature = CryptoJS.SHA256("CTD378Du" + MerID + OrderID).toString(CryptoJS.enc.Base64)
+    let {oid, amount} = req.body
 
-        let amount = parseFloat((parseFloat(amt) / 100.0).toFixed(2));
+    console.log(req.body)
 
-        let data = {
-            "id": OrderID,
-            "orderId": OrderID,
-            "receipt_id": OrderID,
-            "client": {"email": "yumecs.pay@gmail.com"},
-            "company": {
-                "email": "yumecs.pay@gmail.com",
-                "inn": "7726433751",
-                "paymentAddress": "https://yumecs.uz"
-            },
-            "receipt": {
-                "items": [{
-                    "name": OrderID + " " + fio,
-                    "price": amount,
-                    "quantity": 1.0,
-                    "sum": amount,
-                    "unit": encodeURIComponent("шт"),
-                    "method": "full_payment",
-                    "object": "commodity",
-                    "vat": {"type": "none", "sum": 0}
-                }],
-                "payments": [{"type": 1, "sum": amount}],
-                "total": amount
+    let amount_value = parseFloat(amount)
+
+    let signature = CryptoJS.SHA256("CTD378Du" + mid + oid).toString(CryptoJS.enc.Base64)
+
+    let data = {
+        "id": oid,
+        "orderId": oid,
+        "client": {"email": "yumecs.pay@gmail.com"},
+        "company": {
+            "email": "yumecs.pay@gmail.com",
+            "inn": "7726433751",
+            "paymentAddress": "https://yumecs.uz"
+        },
+        "receipt": {
+            "items": [{
+                "name": oid,
+                "price": amount_value,
+                "quantity": 1.0,
+                "sum": amount_value,
+                "unit": encodeURIComponent("шт"),
+                "method": "full_payment",
+                "object": "commodity",
+                "vat": {"type": "none", "sum": 0}
+            }],
+            "payments": [{"type": 1, "sum": amount_value}],
+            "total": amount_value
+        }
+    };
+
+    console.log("Data\n")
+    console.log(data)
+    console.log("======================================")
+
+
+    let receiptResponse = await axios.default.post("https://mpi.mkb.ru:8443/OnlineReceipt/1/" + mid + "/receipt",
+        data,
+        {
+            headers: {
+                "Content-Type": "application/json",
+                "Signature": signature,
+                "Accept-Charset": "utf-8"
             }
-        };
-
-        console.log("Data\n")
-        console.log(data)
-        console.log("======================================")
-
-
-        axios.default.post("https://mpi.mkb.ru:8443/OnlineReceipt/1/" + MerID + "/receipt",
-            data,
-            {
-                headers: {
-                    "Content-Type": "application/json",
-                    "Signature": signature,
-                    "Accept-Charset": "utf-8"
-                }
-            }).then(function (response) {
-            console.log("Response: \n")
-            console.log(JSON.stringify(response.data))
-            res.statusCode = 200
-            res.send({
-                status: "Send successful"
-            })
-        }).catch(function (error) {
-            if (error.response) {
-                // The request was made and the server responded with a status code
-                // that falls out of the range of 2xx
-                console.log(error.response.data);
-                console.log(error.response.status);
-                console.log(error.response.headers);
-            } else if (error.request) {
-                // The request was made but no response was received
-                // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
-                // http.ClientRequest in node.js
-                console.log(error.request);
-            } else {
-                // Something happened in setting up the request that triggered an Error
-                console.log('Error', error.message);
-            }
-            console.log(error.config);
-            res.statusCode = 200
-            res.send({
-                status: "error",
-                error: error
-            })
-        });
-    }else{
-        res.statusCode = 200;
-        res.send({
-            message: "Unsuccessful payment",
-            "request": req.body
         })
+    console.log("Response: \n")
+    console.log(JSON.stringify(receiptResponse.data))
+
+    if (receiptResponse.data["success"]){
+
+        let amount_str = amount + "00"
+
+        while (amount_str.length < 12){
+            amount_str = "0"+amount_str
+        }
+
+        let payment_info = {
+            "mid": mid,
+            "aid": aid,
+            "amount": amount_value,
+            "oid": oid,
+            "signature": CryptoJS.SHA256("CTD378Du"+mid+aid+oid+amount_str+"643").toString(CryptoJS.enc.Base64),
+            "redirect_url": redirect_url,
+            "site_link": site_link,
+            "merchant_mail": merchant_mail,
+            "receipt_id": oid
+        }
+
+        let paymentResponse = axios.default.post("https://mpi.mkb.ru/MPI_payment/",
+            payment_info)
+
+        console.log(JSON.stringify(paymentResponse))
     }
+
+    res.statusCode = 200
+    res.send({
+        status: "Send successful"
+    })
 }
