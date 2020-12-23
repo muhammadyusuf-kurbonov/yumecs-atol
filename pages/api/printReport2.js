724733b40df60c6f2155f70ff8d5544ac8c7ec42
Muhammadyusuf <muhammadyusuf.kurbonov2002@gmail.com>
2020-12-23 22:57:12 +0500

    Split test and prod

---
+++ pages/api/printReport2.js
@@ -0,0 +1,100 @@
+export default async (req, res) => {
+    let axios = require("axios")
+    let CryptoJS = require("crypto-js")
+
+    const mid = "620000000003267"
+    const aid = "443222"
+    const site_link = "https://yumecs.uz"
+    const redirect_url = "www.yumecs.uz"
+    const merchant_mail = "yumecspay@gmail.com"
+
+    res.statusCode = 200
+
+    let {oid, amount, email, phone} = req.body
+
+    console.log(req.body)
+
+    let amount_value = parseFloat(amount)
+
+    let signature = CryptoJS.SHA256("CTD378Du" + mid + oid).toString(CryptoJS.enc.Base64)
+
+    let data = {
+        "id": oid,
+        "orderId": oid,
+        "client": {
+            "email": email,
+            "phone": phone
+        },
+        "company": {
+            "email": "yumecs.uz@gmail.com",
+            "sno":"usn_income_outcome",
+            "inn": "7726433751",
+            "paymentAddress": "https://yumecs.uz"
+        },
+        "receipt": {
+            "items": [{
+                "name": oid,
+                "price": amount_value,
+                "quantity": 1.0,
+                "sum": amount_value,
+                "unit": encodeURIComponent("шт"),
+                "method": "full_payment",
+                "object": "service",
+                "vat": {"type": "none", "sum": 0}
+            }],
+            "payments": [{"type": 1, "sum": amount_value}],
+            "total": amount_value
+        }
+    };
+
+    console.log("Data\n")
+    console.log(data)
+    console.log("======================================")
+
+
+    let receiptResponse = await axios.default.post("https://mpi.mkb.ru:8443/OnlineReceipt/1/" + mid + "/receipt",
+        data,
+        {
+            headers: {
+                "Content-Type": "application/json",
+                "Signature": signature,
+                "Accept-Charset": "utf-8"
+            }
+        })
+    console.log("Response: \n")
+    console.log(JSON.stringify(receiptResponse.data))
+
+    if (receiptResponse.data["result"]["success"]){
+
+        let amount_str = amount + "00"
+
+        while (amount_str.length < 12){
+            amount_str = "0"+amount_str
+        }
+
+        let payment_info = {
+            "mid": mid,
+            "aid": aid,
+            "amount": amount_str,
+            "oid": oid,
+            "signature": CryptoJS.SHA256("CTD378Du"+mid+aid+oid+amount_str+"643").toString(CryptoJS.enc.Base64),
+            "redirect_url": redirect_url,
+            "site_link": site_link,
+            "merchant_mail": merchant_mail,
+            "receipt_id": oid
+        }
+
+        let params = Object.keys(payment_info).map(function(k) {
+            return encodeURIComponent(k) + '=' + encodeURIComponent(payment_info[k])
+        }).join('&')
+        console.log(params)
+        res.redirect("https://mpi.mkb.ru/MPI_payment/?" + params)
+
+    }else{
+        res.send({
+            status: "Send unsuccessful"
+        })
+    }
+
+
+}
